9541f57e9bbc6f9274944ea1911ab46364a3cb60
fenglingnan <185975315@qq.com>
2024-05-22 22:19:16 +0800

    feat: init

---
+++ Assets/Global.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // 核心移动区是宽11高12，外圈一格，左边5格
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

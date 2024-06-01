780df02ce6d42b8f08c8365dccc4d652fd0126a0
linjinxiang <185975315@qq.com>
2024-06-01 17:38:25 +0800

    feat: 基础模板完成

--- Assets/Global.cs
+++ Assets/Global.cs
@@ -6,6 +6,7 @@ public class Global : MonoBehaviour
 {
     // Start is called before the first frame update
     // 核心移动区是宽11高12，外圈一格，左边5格
+    // 黄钥匙Y，蓝钥匙B，红钥匙R，等级Level，生命Health，攻击Attack，防御Defense，金币Coin，经验Exp
     void Start()
     {
         

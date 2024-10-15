b9e9f8377a4933f7ab9973078167c18d7b69274c
fenglingnan <185975315@qq.com>
2024-10-15 22:01:19 +0800

    获取碰撞关键

--- Assets/Script/Health.cs
+++ Assets/Script/Health.cs
@@ -7,7 +7,7 @@ public class Health : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log($"{Global.Instance.Attack}");
+        //Debug.Log($"{Global.Instance.Attack}");
     }
 
     // Update is called once per frame
